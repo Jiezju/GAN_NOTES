d253f75db0bda87f4ae06845f839215db0115c45
Bright <21725100@zju.edu.cn>
2022-06-15 23:23:55 +0800

    [gan] update dtn

--- code/stargan.py
+++ code/stargan.py
@@ -30,4 +30,20 @@ class LossFunc(nn.Module):
                 self.cls_weight * self.classification_loss(d_ori_logit, origin_label)
 
 
+import torch
+import torch.nn as nn
 
+class TVLoss(nn.Module):
+    def __init__(self,TVLoss_weight=1):
+        super(TVLoss,self).__init__()
+        self.TVLoss_weight = TVLoss_weight
+
+    def forward(self,x):
+        batch_size = x.size()[0]
+        h_x = x.size()[2]
+        w_x = x.size()[3]
+        count_h =  (x.size()[2]-1) * x.size()[3]
+        count_w = x.size()[2] * (x.size()[3] - 1)
+        h_tv = torch.pow((x[:,:,1:,:]-x[:,:,:h_x-1,:]),2).sum()
+        w_tv = torch.pow((x[:,:,:,1:]-x[:,:,:,:w_x-1]),2).sum()
+        return self.TVLoss_weight*2*(h_tv/count_h+w_tv/count_w)/batch_size
